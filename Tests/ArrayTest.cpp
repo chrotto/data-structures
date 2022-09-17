5c797db592a3d41bca9a19cb8cbdfd705fcc0bca
Christopher Otto <Christopherotto@wtnet.de>
2022-09-17 20:56:53 +0200

    Add pushing of new array elements and array resizing

--- Tests/ArrayTest.cpp
+++ Tests/ArrayTest.cpp
@@ -1,10 +1,10 @@
 #include "pch.h"
 #include "Array.h"
 
-TEST(Array, InitializeWithDefaultSizeTen) {
+TEST(Array, InitializeWithSizeZero) {
 	DS::Array<int>* arr = new DS::Array<int>();
 
-	EXPECT_EQ(arr->getSize(), 10);
+	EXPECT_EQ(arr->getSize(), 0);
 
 	delete arr;
 }
@@ -13,6 +13,18 @@ TEST(Array, InitializeWithCustomSize) {
 	DS::Array<int>* arr = new DS::Array<int>(20);
 
 	EXPECT_EQ(arr->getSize(), 20);
+
+	delete arr;
+}
+
+TEST(Array, AddElement) {
+	DS::Array<int>* arr = new DS::Array<int>();
+
+	EXPECT_EQ(arr->getSize(), 0);
+	arr->push(1);
+	EXPECT_EQ(arr->getSize(), 1);
+	arr->push(2);
+	EXPECT_EQ(arr->getSize(), 2);
 
 	delete arr;
 }
--- data-structures/Array.h
+++ data-structures/Array.h
@@ -6,30 +6,78 @@ namespace DS
 	class Array
 	{
 	private:
+		T* values;
+		int maxSize;
 		int size;
 
 	public:
 		Array();
 		explicit Array(int size);
+		~Array();
 
 		const int getSize() const;
+
+		void push(T value);
+
+	private:
+		void resize(int newSize);
 	};
 
 	template<typename T>
-	Array<T>::Array() : Array(10)
+	Array<T>::Array() : Array(0)
 	{
 		// Nothing to do
 	}
-	
+
 	template<typename T>
-	Array<T>::Array(int size) : size(size)
+	Array<T>::Array(int size) : maxSize(size), size(size)
 	{
-		// Nothing to do
+		values = new T[maxSize];
+	}
+
+	template<typename T>
+	Array<T>::~Array()
+	{
+		delete[] values;
 	}
 
 	template<typename T>
 	const int Array<T>::getSize() const
 	{
 		return size;
+	}
+
+	template<typename T>
+	void Array<T>::push(T value)
+	{
+		if (size == maxSize)
+		{
+			resize(maxSize + 1);
+		}
+		values[size++] = value;
+	}
+
+	template<typename T>
+	void Array<T>::resize(int newSize)
+	{
+		if (newSize == maxSize)
+		{
+			return;
+		}
+		maxSize = newSize;
+
+		if (newSize < size)
+		{
+			size = newSize;
+		}
+
+		T* newValues = new T[newSize];
+		for (int i = 0; i < size; i++)
+		{
+			newValues[i] = values[i];
+		}
+
+		delete[] values;
+		values = newValues;
 	}
 }
